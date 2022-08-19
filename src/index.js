2ef1aacbdaa4046e789e9367c470dca0d5d003c6
aaron <aaron_merino@outlook.com>
2022-08-19 17:16:00 -0400

    fixed index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Expression } from './expression.js';
 import App from "./App.js";
-
-
-const example = new Expression('(A ^ B)', '^', 
-                          [
-                            new Expression('A', 'A', []),
-                            new Expression('B', 'B', [])
-                          ]);
 
 ReactDOM.render(<App />, document.getElementById("root"));
